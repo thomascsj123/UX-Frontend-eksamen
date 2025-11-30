4e6381b7ae6be36948b3f2168cbb0695d0d45af2
andreasfhr16-netizen <andreasfhr16@gmail.com>
2025-11-30 11:44:05 +0100

    dashboard and loginpage

---
+++ app/api/check-user/route.ts
@@ -0,0 +1,24 @@
+//this route is used to get the username of the logged in user
+import { NextResponse } from "next/server";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
+export async function GET() {
+  const { data, error } = await supabase
+    .from("user-table")
+    .select("username, user_mail, user_password")
+    
+    
+
+  
+  if (error) {
+    return NextResponse.json({ error: error.message }, { status: 400 });
+  }
+
+  return NextResponse.json(data);
+}
+
+
--- app/dashboard-page/page.jsx
+++ app/dashboard-page/page.jsx
@@ -1,35 +1,86 @@
+"use client";
 
 import Link from "next/link";
 import Image from "next/image";
 import ComingBookingCard from "@/app/components/coming-bookings-info";
+import { useState, useEffect } from "react";
+
 
 
 export default function DashboardPage() {
+    const userRole = sessionStorage.getItem("userRole");
+    const userName = sessionStorage.getItem("userName");
+    const userMail = sessionStorage.getItem("userName");
+
+    const [loading, setLoading] = useState(true);
+
+
+
+
+
+
+
+    useEffect(() => {
+
+        const Loader = document.getElementById("loader")
+
+
+        if (loading === true) {
+            Loader.style.display = "flex"; //set the Loaders display value to "flex" to show it
+            setTimeout(() => { Loader.style.display = "none"; }, 1500);
+            setLoading(false); //now when loading is false the the if statemnet cant be run
+        }
+
+        console.log("user has logged in as a", userRole)
+        console.log("user has logged in as ", userName)
+        console.log("user has logged in as ", userMail)
+
+
+        if (userRole === "Underviser") {
+            document.getElementById("lokalekontrol").style.display = "none";
+            document.getElementById("book-undervisningslokale").style.display = "flex";
+            document.getElementById("alle-bookninger").style.display = "flex";
+            document.getElementById("book-mødelokale").style.display = "flex";
+
+        }
+
+        if (userRole === "Studerende") {
+            document.getElementById("lokalekontrol").style.display = "none";
+            document.getElementById("book-undervisningslokale").style.display = "none";
+            document.getElementById("alle-bookninger").style.display = "flex";
+            document.getElementById("book-mødelokale").style.display = "flex";
+
+        }
+
+    });
+
     return (
         <div className="dashboard-page-con">
             <div className="dashboard-logo-con" style={{ marginLeft: "200px" }}>
                 <Link href="/">
-                <Image
-                    src="/timeann-img.png"
-                    alt="TimeAnn logo"
-                    width={200}
-                    height={20}
-                    priority
-                />
+                    <Image
+                        src="/timeann-img2.png"
+                        alt="TimeAnn logo"
+                        width={200}
+                        height={20}
+                        priority
+                    />
                 </Link>
-                
+
+                <p>{userRole}</p>
+
             </div>
             <div className="dashboard-usertitle-con">
 
-                <h1>Velkommen "bruger"</h1>
+                <h1>Velkommen, {userName}</h1>
             </div>
 
             <div className="dashboard-book-btn-con">
 
-                <div className="dashboard-book-btn"><p>Book mødelokale</p></div>
-                <div className="dashboard-book-btn"><p>Book undervisningslokale</p></div>
-                <div className="dashboard-book-btn"><p>Alle bookninger</p></div>
-                <div className="dashboard-book-btn"><p>Lokalekontrol</p></div>
+                <div className="dashboard-book-btn" id="book-mødelokale"><Link href="/mødelokale" ><p>Book mødelokale</p></Link></div>
+                <div className="dashboard-book-btn" id="book-undervisningslokale"><Link href="/undervisningslokale" ><p>Book undervisningslokale</p></Link></div>
+                <div className="dashboard-book-btn" id="alle-bookninger"><Link href="/alle-bookninger" ><p>Se alle bookninger</p></Link></div>
+                <div className="dashboard-book-btn" id="lokalekontrol"><Link href="/lokalekontrol" ><p>Lokalekontrol</p></Link></div>
 
 
             </div>
@@ -42,31 +93,11 @@ export default function DashboardPage() {
 
                 <div className="dashboard-coming-bookings-con">
 
-                    <ComingBookingCard
-                        date="Tirsdag 25/11-2025"
-                        room_id="CL3-CL11"
-                        booked_time="08:00-10:30 UTC"
-                        participants="23"
-                    />
+                    <ComingBookingCard />
 
-                    <ComingBookingCard
-                        date="Tirsdag 25/11-2025"
-                        room_id="CL3-CL11"
-                        booked_time="08:00-10:30 UTC"
-                        participants="23"
-                    />
-
-                    <ComingBookingCard
-                        date="Tirsdag 25/11-2025"
-                        room_id="CL3-CL11"
-                        booked_time="08:00-10:30 UTC"
-                        participants="23"
-                    />
-
-
-
-
-
+                    <div className="loader-con">
+                        <div id="loader"></div>
+                    </div>
 
 
 
